f9e2e83a96a5206e2a1d4199923078a0ebb258a7
Somory2333 <96530359+Somory2333@users.noreply.github.com>
2024-12-02 19:31:36 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  optimizeDeps: {
+    // 开发时 解决这些commonjs包转成esm包
+    include: [
+      "@jiaminghi/c-render",
+      "@jiaminghi/c-render/lib/plugin/util",
+      "@jiaminghi/charts/lib/util/index",
+      "@jiaminghi/charts/lib/util",
+      "@jiaminghi/charts/lib/extend/index",
+      "@jiaminghi/charts",
+      "@jiaminghi/color",
+    ],
+  },
+  build: {
+    // 打包时需要另外处理的commonjs规范的包
+    commonjsOptions: {
+      include: [
+        /node_modules/, // 必须包含
+      ],
+    },
+  },
+
+})
